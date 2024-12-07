d1a79dee558acf3eda4c626e2df9f08939f294c0
anabiax <anabia200113@gmail.com>
2024-12-07 18:31:36 -0300

    estrutura projeto final do modulo de LP

---
+++ README.md
@@ -0,0 +1,8 @@
+# Projeto final do módulo de lógica de programação
+
+Bem vinda ao repositório do projeto final do módulo de lógica de programação.
+
+  1. Faça um fork desse repositório.
+  2. Abra o arquivo `calculadora.js` e siga as instruções para para que as operações basicas passem a ser funcional.
+  3. Abra o arquivo `media.js` e siga as instruções para para que a soma passe a ser funcional.  
+  4. [extra] Por fim, publique no Netlify!
---
+++ js/calculadora.js
@@ -0,0 +1,47 @@
+function somar() {
+  const numero1 = parseFloat(document.getElementById("numero1Soma").value);
+  const numero2 = parseFloat(document.getElementById("numero2Soma").value);
+  // evite mexer no código acima!
+
+  let resultado;
+
+  /* use as constantes numero 1 e numero 2 e em seguida armazene na variavel a operação descrita no nome da função.
+  Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
+  document.getElementById("resultadoSoma").innerHTML = resultado;
+}
+
+function subtrair() {
+  const numero1 = parseFloat(document.getElementById("numero1Sub").value);
+  const numero2 = parseFloat(document.getElementById("numero2Sub").value);
+  // evite mexer no código acima!
+
+  let resultado;
+
+  /* use as constantes numero 1 e numero 2 e em seguida armazene na variavel a operação descrita no nome da função.
+   Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
+  document.getElementById("resultadoSub").innerHTML = resultado;
+}
+
+function multiplicar() {
+  const numero1 = parseFloat(document.getElementById("numero1Mult").value);
+  const numero2 = parseFloat(document.getElementById("numero2Mult").value);
+  // evite mexer no código acima!
+
+  let resultado;
+
+  /* use as constantes numero 1 e numero 2 e em seguida armazene na variavel a operação descrita no nome da função.
+   Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
+  document.getElementById("resultadoMult").innerHTML = resultado;
+}
+
+function dividir() {
+  const numero1 = parseFloat(document.getElementById("numero1Div").value);
+  const numero2 = parseFloat(document.getElementById("numero2Div").value);
+  // evite mexer no código acima!
+
+  let resultado;
+
+  /* use as constantes numero 1 e numero 2 e em seguida armazene na variavel a operação descrita no nome da função.
+   Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
+  document.getElementById("resultadoDiv").innerHTML = resultado;
+}
